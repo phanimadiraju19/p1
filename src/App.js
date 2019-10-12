c90f5074a9c8682949fa5ac7b419082e3cc405e1
phanimadiraju19 <phanimadiraju19@gmail.com>
2019-10-12 17:33:45 +0530

    Styled ‘Header’ (Inline Styling)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Header from "./Header";
 function App() {
   return (
     <div>
+      <div style={{textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase'}}>
       <Header/>
+</div>
+      
       <button>Add</button>
       <div>
         <span>Name</span><br />
